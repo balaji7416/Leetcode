d874349c07e5fd7036c4bfcd4a8cb3d288ccad4b
balaji7416 <balaji.nani12345@gmail.com>
2026-01-29 22:48:42 +0530

    dp: solve LeetCode 416 (Partition Equal Subset Sum) using memoization

---
+++ dp/416_partition_equal_subset_sum.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    Problem 416: Partition Equal Subset Sum
+    Pattern: dp
+    Time: O(m*n), Space: O(m*n)
+*/
+
+/*
+    idea: equal subset sum => check whether there exits a subset with sum equal to total_sum/2
+*/
+/*
+    this is top to bottom tabulation
+*/
+class Solution
+{
+public:
+    bool solve(int i, int sum, int total_sum, vector<int> &nums, vector<vector<int>> &dp)
+    {
+        if (sum == total_sum / 2)
+            return true;
+        if (i == nums.size() || sum > total_sum / 2)
+        {
+            return false;
+        }
+        if (dp[i][sum] != -1)
+            return dp[i][sum];
+        bool take = solve(i + 1, sum + nums[i], total_sum, nums, dp);
+        bool not_take = solve(i + 1, sum, total_sum, nums, dp);
+        return dp[i][sum] = take || not_take;
+    }
+    bool canPartition(vector<int> &nums)
+    {
+        int n = nums.size();
+        int total_sum = accumulate(nums.begin(), nums.end(), 0);
+        if (total_sum % 2 != 0)
+            return false;
+        vector<vector<int>> dp(n, vector<int>(total_sum + 1, -1));
+        return solve(0, 0, total_sum, nums, dp);
+    }
+};
