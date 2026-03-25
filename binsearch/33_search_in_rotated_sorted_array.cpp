735f91acb1f44c32a72d113ac48938e155b507ca
balaji7416 <balaji.nani12345@gmail.com>
2026-03-25 13:19:10 +0530

    LC 33: search in rotated sorted arr (binsearch)

---
+++ binsearch/33_search_in_rotated_sorted_array.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    Problem 33: Search in Rotated Sorted Array
+    Pattern: Binary Search
+    Time: O(logn), Space: O(1)
+*/
+
+/*IDEA:
+Find pivot (smallest element) via binary search
+Array splits into two sorted halves
+Run binary search on both halves to find target
+→ overall O(log n)
+*/
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid;
+            }
+        }
+        int pos = low;
+        auto bin_s = [&](int l, int h, int k)
+        {
+            while (l <= h)
+            {
+                int m = l + (h - l) / 2;
+                if (nums[m] == k)
+                    return m;
+                if (nums[m] < k)
+                    l = m + 1;
+                else
+                    h = m - 1;
+            }
+            return -1;
+        };
+        int l_ = bin_s(0, pos - 1, target);
+        int r_ = bin_s(pos, n - 1, target);
+        int ans_ = l_ != -1 ? l_ : r_;
+        return ans_;
+    }
+};
